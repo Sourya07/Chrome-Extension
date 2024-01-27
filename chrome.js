45b421aa8a8d6de2abdc759855c8bd28c9bf9f1f
Sourya07 <singhsourya137@gmail.com>
2024-01-27 18:25:19 +0530

    updating files

--- chrome.js
+++ chrome.js
@@ -1,21 +1,44 @@
 let arr = [];
+let oldLeads = []
 const inputEl = document.getElementById("field");
 const events = document.getElementById("button");
 const ulEl = document.getElementById("ul");
+const deleteBtn = document.getElementById("delete-btn")
+
+// localStorage.setItem("arr","sourya")
+// let namee=localStorage.getItem("arr")
+// console.log(namee)
+// localStorage.clear()
+
+let leadsFromlocakstorage=JSON.parse(localStorage.getItem("arr") )
+if (leadsFromlocakstorage) {
+    arr = leadsFromlocakstorage
+   foam(arr)
+}
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    arr = []
+   foam()
+})
+
+
 
 events.addEventListener("click", function () {
     let values = inputEl.value;
     arr.push(values);
-    foam();
     inputEl.value = "";
+    localStorage.setItem("arr",JSON.stringify(arr))
+    foam();
+    
 });
 
-function foam() {
+function foam(leads) {
     let listitem = "";
     for (let i = 0; i < arr.length; i++) {
 
         listitem += `<li>
-        <a target='_blank' href='${arr[i]}'>${arr[i]}</a></li>`;
+        <a target='_blank' href='${arr[i]}'>
+        ${arr[i]}</a></li>`;
     }
     ulEl.innerHTML = listitem;
 }
